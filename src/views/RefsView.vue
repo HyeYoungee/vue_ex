378c18829d9de432ce2de685ed9bc0759ba9a194
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-10-26 10:36:17 +0900

    231026 [Add] Components Basics

---
+++ src/views/RefsView.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        'html','css', 'javascript'
+      ]
+    }
+  },
+  mounted() {
+    this.$refs.input.focus()
+    // document.querySelector('#username').focus()
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<template>
+
+  <div class="layout">
+    <h1>This is an Refs page</h1>
+    <input ref="input">
+    <!-- <input type="text" id="username"> -->
+
+    <hr>
+    <ul>
+      <li v-for="item in list" ref="items" :key="item.id">
+        {{ item }}
+      </li>
+    </ul>   
+
+  </div>
+</template>
+
+<style>
+</style>
